28da5cf53e64cfe003ca508a5d460769693a89ec
RaeKyo <tienhanv@gmail.com>
2021-06-04 11:43:05 +0700

    Modified W3b1 and w3b9

--- Workshop3/w3b1_PrimeSequence.cpp
+++ Workshop3/w3b1_PrimeSequence.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-// PRIME NUMBER SEQUENCE
+// PRIME NUMBER SEQUENCE FROM 2 TO N
 int checkPrime(int);
 void printPrime(int);
 
 int main(){
 	int n;
-	printf("Enter the number  : ");
-	scanf("%d",&n);
+	do{
+		printf("Enter the number  : ");
+		scanf("%d",&n);
+	}while(n<2);
 	printPrime(n);
 	
 	getchar();
@@ -15,10 +17,10 @@ int main(){
 }
 
 
-int checkPrime(int v){
+int checkPrime(int n){
 	int res =1;
-	for (int i=2;i<=sqrt(v);i++){
-		if (v%i==0) res=0;
+	for (int i=2;i<=sqrt(n);i++){
+		if (n%i==0) res=0;
 	}
 	return res;
 }
@@ -26,7 +28,7 @@ int checkPrime(int v){
 void printPrime(int n){
 	int count=1;
 	int value =2; // initial value from 2
-	while(count<=n){
+	while(count<n){
 		if(checkPrime(value)==1){
 			printf("%4d",value);
 			count++;
